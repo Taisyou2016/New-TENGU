8a8baadd619ec519808552fb470b3d46eebaecef
tunomizu <tumikihurai46@gmail.com>
2016-06-22 16:20:26 +0900

    アイテムいろいろ

---
+++ Assets/Tunoka/Script/Gimmick/Mp_Heel_Point.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mp_Heel_Point : MonoBehaviour {
+
+    private float mp;
+    public float UpTime;
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mp += 0.1f;
+            if (mp >= UpTime)
+            {
+                other.GetComponent<PlayerStatus>().currentMp += 1;
+                mp = 0;
+            }
+        }
+    }
+}
