c017d51bea90c1835f7d19f26de79eaf26fd1fa7
tunomizu <tumikihurai46@gmail.com>
2016-07-08 10:32:39 +0900

    いろいろ

--- Assets/Tunoka/Script/Tornado/Tornado.cs
+++ Assets/Tunoka/Script/Tornado/Tornado.cs
@@ -5,23 +5,29 @@ public class Tornado : MonoBehaviour {
 
 
     [SerializeField]
-    private int Time = 20;
+    private int time = 20;
     public Vector3 TornadoSize = new Vector3(2,4,2);
+
+    [SerializeField, Header("回転スピード")]
+    private float SpinSpeed = 1;
+    
 
     public bool Free = false;
     // Use this for initialization
     void Start()
     {
-        iTween.ScaleTo(gameObject, iTween.Hash("x", TornadoSize.x, "y", TornadoSize.y, "z", TornadoSize.z, "time", Time));
+        iTween.ScaleTo(gameObject, iTween.Hash("x", TornadoSize.x, "y", TornadoSize.y, "z", TornadoSize.z, "time", time));
 
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0f, SpinSpeed * 100 * Time.deltaTime, 0f);
+
         if (transform.localScale.x == TornadoSize.x)
         {
-            if (Free == true)
-            { return; }
+            if (Free == true) return;
             iTween.ScaleTo(gameObject, iTween.Hash("x", 0, "z", 0, "time", 10));
             transform.FindChild("Particle").gameObject.GetComponent<ParticleTornado>().FadeOut(5f);
         }
