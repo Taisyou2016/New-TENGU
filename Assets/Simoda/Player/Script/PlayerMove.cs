3330e50cbd6523b440731d43f0a0f11898ec872a
Syuvi <durchlaufregal@gmail.com>
2016-07-08 13:38:00 +0900

    滞空処理　修正

--- Assets/Simoda/Player/Script/PlayerMove.cs
+++ Assets/Simoda/Player/Script/PlayerMove.cs
@@ -11,6 +11,9 @@ public class PlayerMove : MonoBehaviour
     public float lockOnRotateSpeed = 45.0f; //ロックオンしているときの横移動
     public float lockDistanceY = 10.0f;
     public float gravity = 10.0f; //重力加速度
+    public float flightGravity = 10.0f; //滞空中の重力加速度
+    public float flightGravityDeltaTimeMagnification = 0.1f; //滞空中の重力の計算に使うdeltaTimeに掛ける倍率
+    public float flightGroundDistance = 5.0f; //滞空可能な地面との距離
     public float jampPower = 10.0f; //ジャンプするパワー
     public float knockBackPower = 0.0f; //KnockBackLargeの時吹き飛ぶパワー
     public bool knockBackState = false;
@@ -30,6 +33,8 @@ public class PlayerMove : MonoBehaviour
     private GameObject cameraController;
     private Vector3 velocity;
     private float velocityY = 0;
+    private float gravityDeltaTimeMagnification = 1.0f; //重力の計算に使うdeltaTimeに掛ける倍率
+    private float groundDistance; //プレイヤーの地面までの距離
     private bool jampState = false;
     private bool flightState = false;
     private bool windMove = false;
@@ -72,17 +77,36 @@ public class PlayerMove : MonoBehaviour
     {
         stateProcessor.Execute(); //設定されている移動状態を実行
 
-        if (currentGroundHit == false && flightState == false) velocityY -= gravity * Time.deltaTime;
+        //if (currentGroundHit == false && flightState == false) velocityY -= gravity * Time.deltaTime;
+        //velocity.y = velocityY;
+
+        if (currentGroundHit == false && flightState == false) velocityY -= gravity * Time.deltaTime * gravityDeltaTimeMagnification;
         velocity.y = velocityY;
 
-        if (knockBackState == false && windMove == false && jampState == true && velocityY <= -1.0f && playerStatus.MpCostDecision(playerStatus.flightCost) && Input.GetKey(KeyCode.Space)) //滞空処理
+        if (knockBackState == false
+            && windMove == false
+            && jampState == true
+            && velocityY <= -1.0f
+            && playerStatus.MpCostDecision(playerStatus.flightCost)
+            && groundDistance >= flightGroundDistance
+            && Input.GetKey(KeyCode.Space)) //滞空処理
         {
             playerStatus.MpConsumption(playerStatus.flightCost);
             flightState = true;
-            velocity.y = 0;
+            gravityDeltaTimeMagnification = 0.1f;
+            gravity = flightGravity;
+            //velocity.y = 0;
         }
-        if ((flightState == true && Input.GetKeyUp(KeyCode.Space)) || !playerStatus.MpCostDecision(playerStatus.flightCost))
+        if ((flightState == true && Input.GetKeyUp(KeyCode.Space))
+            || knockBackState == true
+            || windMove == true
+            || !playerStatus.MpCostDecision(playerStatus.flightCost)
+            || groundDistance < flightGroundDistance)
+        {
             flightState = false;
+            gravityDeltaTimeMagnification = 1.0f;
+            gravity = 10.0f;
+        }
 
         playerAnimator.SetBool("FlightState", flightState);
 
@@ -126,6 +150,7 @@ public class PlayerMove : MonoBehaviour
 
 
         //地面との判定　ジャンプ処理
+        groundDistance = CheckGroundDistance();
         previosGroundHit = currentGroundHit; //ひとつ前の状態
         currentGroundHit = CheckGrounded(); //現在の状態
         if (previosGroundHit == false && currentGroundHit == true)
@@ -237,19 +262,43 @@ public class PlayerMove : MonoBehaviour
 
     public bool CheckGrounded() //地面に接地しているかどうかを調べる
     {
-        //controller.isGroundedがtrueならRaycastを使わずに判定終了
-        //if (controller.isGrounded) return true;
-        //放つ光線の初期位置と姿勢
+        ////controller.isGroundedがtrueならRaycastを使わずに判定終了
+        ////if (controller.isGrounded) return true;
+        ////放つ光線の初期位置と姿勢
+        ////若干体にめり込ませた位置から発射しないと正しく判定できないときがある
+        //Ray ray = new Ray(transform.position + Vector3.up * 0.1f, Vector3.down);
+        ////探索距離
+        //float tolerance = 1.3f;
+        //Debug.DrawRay(ray.origin, ray.direction * tolerance);
+        ////Raycastがhitするかどうかで判定
+        ////RaycastHit hit;
+        ////地面にのみ衝突するようにレイヤを指定する
+        //return Physics.Raycast(ray, tolerance, 1 << 8);
+        ////return Physics.BoxCast(ray.origin, new Vector3(0.1f, 0.05f, 0.1f), ray.direction, transform.rotation, tolerance, 1 << 8);
+
+        if (groundDistance < 1.10)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public float CheckGroundDistance()
+    {
         //若干体にめり込ませた位置から発射しないと正しく判定できないときがある
         Ray ray = new Ray(transform.position + Vector3.up * 0.1f, Vector3.down);
         //探索距離
-        float tolerance = 1.3f;
+        float tolerance = Mathf.Infinity;
         Debug.DrawRay(ray.origin, ray.direction * tolerance);
         //Raycastがhitするかどうかで判定
-        //RaycastHit hit;
+        RaycastHit hit;
         //地面にのみ衝突するようにレイヤを指定する
-        return Physics.Raycast(ray, tolerance, 1 << 8);
-        //return Physics.BoxCast(ray.origin, new Vector3(0.1f, 0.05f, 0.1f), ray.direction, transform.rotation, tolerance, 1 << 8);
+        Physics.Raycast(ray, out hit, tolerance, 1 << 8);
+
+        return (transform.position - hit.point).magnitude;
     }
 
     public bool GetLockOnInfo()
@@ -489,7 +538,7 @@ public class PlayerMove : MonoBehaviour
 
         windPower -= Time.deltaTime;
         if (Input.GetAxis("Vertical") < 0)
-            windPower -= Time.deltaTime * 10;
+            windPower -= Time.deltaTime * 20;
 
         transform.LookAt(transform.position + velocity);
         transform.Rotate(new Vector3(0, 0, 1), -15 * Input.GetAxis("Horizontal"));
